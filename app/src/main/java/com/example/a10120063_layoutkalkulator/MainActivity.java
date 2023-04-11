71821bc49d39b187c54b97c861c0d9d1c277376b
Inka Warijo <127416514+ngodingmace@users.noreply.github.com>
2023-04-12 06:22:09 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/a10120063_layoutkalkulator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a10120063_layoutkalkulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
